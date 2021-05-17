9b98261bf91121dda893a5d60ddd02d4e82b42b7
VladUgarov <69145359+VladUgarov@users.noreply.github.com>
2021-05-17 22:55:12 +0300

    Lesson5

---
+++ Lesson5.js
@@ -0,0 +1,55 @@
+class Stack{
+    constructor(maxSize) {
+        this.maxSize = maxSize
+        if(this.maxSize === undefined){
+            this.maxSize = 10
+            this.storage = new Array(this.maxSize);
+
+        }
+
+        else if(typeof this.maxSize !== "number" ||  !isFinite(this.maxSize)){
+            throw new Error("Введенное число невалидно")
+        }
+        else{
+            this.maxSize = maxSize
+            this.storage = new Array(this.maxSize)
+        }
+        this.current = 0;
+
+    }
+    push = (elem) => {
+        if(this.current >= this.maxSize){
+            throw new Error("Cтек переполнен")
+        }
+        this.storage[this.current] = elem
+        this.current++
+    }
+    pop = () => {
+        if(this.current === 0){
+            throw new Error("Стек пуст")
+        }
+        this.current--
+        delete this.storage[this.current]
+    }
+    peek = () => {
+        if(this.current === 0){
+            return null
+        }
+        return this.storage[this.current-1]
+    }
+    isEmpty = () => {
+        if(this.current ===0){
+            return true
+        }else{
+            return false
+        }
+    }
+
+}
+
+let stack = new Stack(3)
+stack.push(2)
+
+
+console.log(stack.isEmpty())
+console.log(stack.peek())
